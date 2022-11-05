1fa28fe466ae8f1f225c4dfc6034b20ff01b8638
function07 <houssine199222@gmail.com>
2022-11-05 00:05:41 -0400

    Add functionality for operands, operators, and clear button

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const display = document.querySelector('#display');
+const operandButtons = document.querySelectorAll('.number-btn');
+const operatorButtons = document.querySelectorAll('.operator-btn');
+const equalButton = document.querySelector('#equal')
+const decimal = document.querySelector('#decimal');
+const clear = document.querySelector('#clear');
+let arrayX = [];
+let arrayY = [];
+let operator = null;
+
 function add(x, y){
     return x+y
 };
@@ -19,12 +29,58 @@ function operate(operator, x, y){
         return add(x, y);
     }
     else if (operator == "subtract"){
-        return subtract( x, y);
+        return subtract(x, y);
     } 
     else if (operator == "multiply"){
         return multiply(x, y);
     }
-    else {
-        return divide(x, y);
-    };
+    else if(operator === "divide") 
+        return divide(x, y);   
 };
+
+function storeVariableX(){
+    x = Number(arrayX.join(''));
+    display.textContent = Number(arrayX.join('')); 
+};
+
+function storeVariableY(){
+    y = Number(arrayY.join(''));
+    display.textContent = Number(arrayY.join(''));
+};
+
+operandButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+
+        if(operator === null){
+            arrayX.push(e.target.textContent);
+            storeVariableX();
+            console.log(arrayX); 
+        }
+        else if(operator !== null){
+            arrayY.push(e.target.textContent);
+            storeVariableY();
+            console.log(arrayY); 
+        }  
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        
+        operator = e.target.textContent
+
+        if(operator !== null){
+            display.textContent = Number(arrayX.join(''));
+        }
+    });
+});
+
+clear.addEventListener('click', () => {
+    arrayX = [];
+    arrayY = [];
+    operator = null;
+    x = ''
+    y = ''
+    display.textContent = '0';
+});
+ 
